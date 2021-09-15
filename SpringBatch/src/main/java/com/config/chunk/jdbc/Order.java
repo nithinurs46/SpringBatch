f7a857547c081b1e5ad80c8a39ca450cd37c46d3
Nithin Urs <70891889+nithinurs46@users.noreply.github.com>
2021-09-15 12:00:09 +0530

    Added status field and used lombok

--- SpringBatch/src/main/java/com/config/chunk/jdbc/Order.java
+++ SpringBatch/src/main/java/com/config/chunk/jdbc/Order.java
@@ -5,6 +5,11 @@ import java.util.Date;
 
 import javax.validation.constraints.Pattern;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Order {
 
 	private Long orderId;
@@ -23,70 +28,8 @@ public class Order {
 	private String itemName;
 
 	private Date shipDate;
-
-	public Long getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public BigDecimal getCost() {
-		return cost;
-	}
-
-	public void setCost(BigDecimal cost) {
-		this.cost = cost;
-	}
-
-	public String getItemId() {
-		return itemId;
-	}
-
-	public void setItemId(String itemId) {
-		this.itemId = itemId;
-	}
-
-	public String getItemName() {
-		return itemName;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	public Date getShipDate() {
-		return shipDate;
-	}
-
-	public void setShipDate(Date shipDate) {
-		this.shipDate = shipDate;
-	}
+	
+	private String status;
 
 	@Override
 	public String toString() {
